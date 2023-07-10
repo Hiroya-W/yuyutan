a69d1de4c2f3920bffd4d75583cf9f2e1d0da358
Hiroya-W <hyuyukun@gmail.com>
2023-07-11 02:03:09 +0900

    :sparkles: Run follow back script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,3 +5,4 @@ set -e
 poetry install
 poetry run rq worker --url redis://redis:6379 --with-scheduler &
 poetry run python workers/periodic_toot.py &
+poetry run python yuyutan/jobs/follow_back.py &
