eb923ea0e72b9e66243678259a11144a228c1f72
Hiroya-W <hyuyukun@gmail.com>
2023-07-12 17:46:54 +0900

    :+1: Fix follow back script path

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 poetry install
 poetry run rq worker --url redis://redis:6379 --with-scheduler &
 poetry run python workers/periodic_toot.py &
-poetry run python yuyutan/jobs/follow_back.py &
+poetry run python workers/follow_back.py &
