2d5a67549673d4afc9503c113a5b6e2789c22b71
Hiroya-W <hyuyukun@gmail.com>
2023-07-15 14:40:40 +0900

    :+1: Use stream handlers

--- mastodon_bot/core.py
+++ mastodon_bot/core.py
@@ -1,8 +1,10 @@
+import time
 from os import _Environ
 
 from sqlalchemy.orm import Session
 
 from mastodon_bot.mastodon_bot import MastodonBot
+from mastodon_bot.streaming import FollowingHandler, ReplyHandler
 
 
 class Bot:
@@ -10,9 +12,18 @@ class Bot:
         self.__env = env
         self.__db = db
         self.__mastodon_bot = MastodonBot(env=env)
+        self.__mastodon_bot.add_listener_many(
+            [
+                FollowingHandler(),
+                ReplyHandler(),
+            ]
+        )
 
     def run(self) -> None:
         """
         Run the bot
         """
+
         self.__mastodon_bot.run()
+        while True:
+            time.sleep(1)
