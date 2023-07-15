c6482ff65fe1fd0e329a3fb2f3a6008f39204dc5
Hiroya-W <hyuyukun@gmail.com>
2023-07-15 10:40:01 +0900

    :sparkles: Create session

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ Bot entry point
 
 import os
 from dotenv import load_dotenv
+from mastodon_bot.databases.mysql import create_session
 
 
 load_dotenv()
 
 
 if __name__ == "__main__":
+    session = create_session(
+        username=os.getenv("MYSQL_USERNAME", "root"),
+        password=os.getenv("MYSQL_PASSWORD", "root"),
+        host=os.getenv("MYSQL_HOST", "localhost"),
+        port=int(os.getenv("MYSQL_PORT", 3306)),
+        database=os.getenv("MYSQL_DATABASE", "mastodon_bot"),
+    )
     print(os.environ)
