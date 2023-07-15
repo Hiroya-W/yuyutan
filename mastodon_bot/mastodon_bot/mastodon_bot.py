63af851003bfb31c4c0de59246aa35c02a0bfe63
Hiroya-W <hyuyukun@gmail.com>
2023-07-15 14:40:27 +0900

    :sparkles: Set handlers

--- mastodon_bot/mastodon_bot/mastodon_bot.py
+++ mastodon_bot/mastodon_bot/mastodon_bot.py
@@ -1,6 +1,9 @@
 from os import _Environ
+from typing import Any
 
 from mastodon import Mastodon
+
+from mastodon_bot.mastodon_bot.streaming import BotStreamListener, CallbackStreamListener
 
 
 class MastodonBot:
@@ -12,9 +15,35 @@ class MastodonBot:
             client_secret=env.get("MASTODON_CLIENT_SECRET", "mastodon_client_secret"),
             access_token=env.get("MASTODON_ACCESS_TOKEN", "mastodon_access_token"),
         )
+        self.__listener = BotStreamListener()
+
+        self.__middlewares: list[Any] = []
+
+    def add_middleware(self, middleware: list[Any]) -> None:
+        """
+        Add a middleware to the bot
+        """
+        self.__middlewares.append(middleware)
+
+    def add_listener(self, listener: CallbackStreamListener) -> None:
+        """
+        Add a listener to the bot
+        """
+        self.__listener.add_handler(listener)
+
+    def add_listener_many(self, listeners: list[CallbackStreamListener]) -> None:
+        """
+        Add multiple listeners to the bot
+        """
+        for listener in listeners:
+            self.add_listener(listener)
 
     def run(self) -> None:
         """
         Run the bot
         """
-        raise NotImplementedError
+        self.__api.stream_user(
+            self.__listener,
+            run_async=True,
+            reconnect_async=True
+        )
