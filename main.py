88c407797e0f14fe8f0d90551313d08d46b0daa5
Hiroya-W <hyuyukun@gmail.com>
2023-07-22 10:36:19 +0900

    :books: Add comment

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ Bot entry point
 import os
 
 from dotenv import load_dotenv
-from mastodon_bot.core import Bot
 
+from mastodon_bot.core import Bot
 from mastodon_bot.databases.mysql import create_session
 
 load_dotenv()
 
 
 if __name__ == "__main__":
+    # DBが変わっても、同じようにSQLAlchemyのSession型で利用できる
     session = create_session(
         username=os.getenv("MYSQL_USERNAME", "root"),
         password=os.getenv("MYSQL_PASSWORD", "root"),
