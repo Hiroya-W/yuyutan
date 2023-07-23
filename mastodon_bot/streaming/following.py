800e5fd9cb06acb26e62e114444c94f75717a8c4
Hiroya-W <hyuyukun@gmail.com>
2023-07-23 23:57:57 +0900

    :+1: Get redis queue scheduler instance

--- mastodon_bot/streaming/following.py
+++ mastodon_bot/streaming/following.py
@@ -1,6 +1,7 @@
 import logging
 
 from mastodon.types import Notification
+from rq_scheduler import Scheduler
 
 from mastodon_bot.mastodon_bot.mastodon_bot import BotInterface
 from mastodon_bot.mastodon_bot.streaming import CallbackStreamListener
@@ -9,10 +10,11 @@ logger = logging.getLogger(__name__)
 
 
 class FollowingHandler(CallbackStreamListener):
-    def __init__(self, bot_instance: BotInterface) -> None:
+    def __init__(self, bot_instance: BotInterface, scheduler: Scheduler) -> None:
         super().__init__()
         self.__bot = bot_instance
         self.__api = bot_instance.get_api_instance()
+        self.__scheduler = scheduler
 
     def on_notification(self, notification: Notification) -> None:
         if notification.type == "follow":
